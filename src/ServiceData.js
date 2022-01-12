7b2fba3a85e19b3fa7e391bce18452d5dcae5b49
Pankaj Yadav <91059277+iampankaj0@users.noreply.github.com>
2022-01-12 11:47:46 +0530

    v
    v

---
+++ src/ServiceData.js
@@ -0,0 +1,90 @@
+import ImageA from './images/image-1.webp';
+import ImageB from './images/img-2.webp';
+import ImageC from './images/img-3.webp';
+import ImageD from './images/img-4.webp';
+import ImageE from './images/img-5.webp';
+import ImageF from './images/img-6.jpg';
+import ImageG from './images/img-7.webp';
+import ImageH from './images/img-8.webp';
+import ImageI from './images/img-9.jpg';
+import ImageJ from './images/img-10.jpg';
+import ImageK from './images/img-11.jpg';
+import ImageL from './images/img-12.jpg';
+
+
+const ServiceData = () => [
+    {
+        id: 1,
+        imgsec: ImageA,
+        textData: 'first'
+    },
+
+    {
+        id: 2,
+        imgsec: ImageB,
+        textData: 'Second'
+    },
+
+    {
+        id: 3,
+        imgsec: ImageC,
+        textData: 'Third'
+    },
+
+    {
+        id: 4,
+        imgsec: ImageD,
+        textData: 'forth'
+    },
+
+    {
+        id: 5,
+        imgsec: ImageE,
+        textData: 'Fifth'
+    },
+
+    {
+        id: 6,
+        imgsec: ImageF,
+        textData: 'Sixth'
+    },
+
+    {
+        id: 7,
+        imgsec: ImageG,
+        textData: 'Sevevnth'
+    },
+
+    {
+        id: 8,
+        imgsec: ImageH,
+        textData: 'Eighth'
+    },
+
+    {
+        id: 9,
+        imgsec: ImageI,
+        textData: 'Nineth'
+    },
+
+    {
+        id: 10,
+        imgsec: ImageJ,
+        textData: 'tenth'
+    },
+
+    {
+        id: 11,
+        imgsec: ImageK,
+        textData: 'Eleventh'
+    },
+
+    {
+        id: 12,
+        imgsec: ImageL,
+        textData: 'Twelveth'
+    },
+
+]
+
+export default ServiceData;
---
+++ src/Services.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <>
+            <section className="services">
+                <h1 className="heading text-center">our services</h1>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-3">pankaj</div>
+                        <div className="col-3">pankaj</div>
+                        <div className="col-3">pankaj</div>
+                        <div className="col-3">pankaj</div>
+                        <div className="col-3">pankaj</div>
+                        <div className="col-3">pankaj</div>
+                        <div className="col-3">pankaj</div>
+                        <div className="col-3">pankaj</div>
+                        <div className="col-3">pankaj</div>
+                        <div className="col-3">pankaj</div>
+                        <div className="col-3">pankaj</div>
+                        <div className="col-3">pankaj</div>
+                        <div className="col-3">pankaj</div>
+                        <div className="col-3">pankaj</div>
+                        <div className="col-3">pankaj</div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default Services;
